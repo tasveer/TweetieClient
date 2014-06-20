06001ecc1f2fc101041db0121200bdcf011c3348
Hunaid <hunaid@hotmail.com>
2014-06-19 21:58:07 -0700

    Initial Commit

---
+++ TweetieClientTests/TweetieClientTests.m
@@ -0,0 +1,34 @@
+//
+//  TweetieClientTests.m
+//  TweetieClientTests
+//
+//  Created by Hunaid Hussain on 6/19/14.
+//  Copyright (c) 2014 kolekse. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TweetieClientTests : XCTestCase
+
+@end
+
+@implementation TweetieClientTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
